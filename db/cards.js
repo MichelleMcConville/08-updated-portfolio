c8c128b840985868d1f6f72967b07e84a105b663
Michelle McConville <mchelmconvl@gmail.com>
2020-11-04 11:57:29 -0600

    initial commit

---
+++ db/cards.js
@@ -0,0 +1,66 @@
+var sites = [
+    {
+      url: "https://michellemcconville.github.io/P1-camping-spot-finder/",
+      imageLink: "./images/camping320.jpg",
+      alt: "camping-app",
+      title: "Camping Spot 🏕️Finder",
+      githubLink: "https://github.com/MichelleMcConville/P1-camping-spot-finder",
+      subText: "Click to get code",
+      desc: "Looking for a place to camp in the National Parks! Use this app."
+    },
+    {
+      url: "https://michellemcconville.github.io/06-weather-dashboard/",
+      imageLink: "./images/weather320.jpg",
+      alt: "weather-app",
+      title: "Weather 🌤️ Dashboard",
+      githubLink: "https://github.com/MichelleMcConville/06-weather-dashboard",
+      subText: "Click to get code",
+      desc: "Check today's weather & the 5-Day forecast for a city near you!"
+    },
+    {
+      url: "https://michellemcconville.github.io/05-work-day-scheduler/",
+      imageLink: "./images/dayPlanner320.jpg",
+      alt: "day-planner-app",
+      title: "Day 📆 Planner",
+      githubLink: "https://github.com/MichelleMcConville/05-work-day-scheduler",
+      subText: "Click to get code",
+      desc: "Keep track of your workday schedule with this handy work day planner!"
+    },
+    {
+      url: "https://michellemcconville.github.io/04-code-quiz/",
+      imageLink: "./images/quizStart320.jpg",
+      alt: "code-quiz",
+      title: "Code ⏲️ Quiz",
+      githubLink: "https://github.com/MichelleMcConville/04-code-quiz",
+      subText: "Click to get code",
+      desc: "Test your knowledge against your friends with this fun code quiz!"
+    },
+    {
+      url: "https://michellemcconville.github.io/03-password-generator/",
+      imageLink: "./images/pwGen320.jpg",
+      alt: "password-generator-app",
+      title: "Password 🔒 Generator",
+      githubLink: "https://github.com/MichelleMcConville/03-password-generator",
+      subText: "Click to get code",
+      desc: "Need help generating a password? Use this app to create a secure password!"
+    },
+    {
+      url: "https://michellemcconville.github.io/bookmarks/",
+      imageLink: "./images/bookmarksDark.jpg",
+      alt: "html-bookmarks",
+      title: "Favorites 🌸 Page",
+      githubLink: "https://github.com/MichelleMcConville/bookmarks",
+      subText: "Click to get code",
+      desc: "A template to use for organizing all your bookmarked sites into a fun 1-page format!"
+    }
+  ];
+  
+    // {
+    //   "url: "TBD",
+    //   "imageLink: "TBD",
+    //   "alt: "TBD",
+    //   "title: "TBD",
+    //   "githubLink: "TBD",
+    //   "subText: "TBD",
+    //   "desc: "TBD"
+    // },
