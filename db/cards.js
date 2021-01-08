9c9dafd6882dbd2854e6e7b00e4cef57192e9006
Michelle McConville <mchelmconvl@gmail.com>
2021-01-07 22:09:28 -0600

    added card w/ project #2 info

--- db/cards.js
+++ db/cards.js
@@ -1,104 +1,113 @@
 var sites = [
-    {
-      url: "https://michellemcconville.github.io/P1-camping-spot-finder/",
-      imageLink: "./images/camping320.jpg",
-      alt: "camping-app",
-      title: "Camping Spot 🏕️Finder",
-      githubLink: "https://github.com/MichelleMcConville/P1-camping-spot-finder",
-      subText: "Click to get code",
-      desc: "Looking for a place to camp in the National Parks! Use this app."
-    },
-    {
-      url: "https://mchel-burger-app.herokuapp.com/",
-      imageLink: "./images/burgerApp320.jpg",
-      alt: "burger-logger",
-      title: "Burger 🍔 Logger",
-      githubLink: "https://github.com/MichelleMcConville/13-burger",
-      subText: "Click to get code",
-      desc: "Keep track of any & all burgers you have tried!"
-    },
-    {
-      url: "https://github.com/MichelleMcConville/12-employee-tracker",
-      imageLink: "./images/employeeTracker320.jpg",
-      alt: "employee-list",
-      title: "Employee 🧑‍🤝‍🧑 Tracker",
-      githubLink: "https://github.com/MichelleMcConville/12-employee-tracker",
-      subText: "Click to get code",
-      desc: "Use this CLI app to maintain a list of employees for your company!"
-    },
-    {
-      url: "https://mchel-note-taker.herokuapp.com/",
-      imageLink: "./images/noteTaker320.jpg",
-      alt: "note-taker-app",
-      title: "Note 📝 Taker",
-      githubLink: "https://github.com/MichelleMcConville/11-note-taker",
-      subText: "Click to get code",
-      desc: "Save paper by using this cute little notes app!"
-    },
-    {
-      url: "https://github.com/MichelleMcConville/10-employee-summary/",
-      imageLink: "./images/employeeSummary320.jpg",
-      alt: "team-list",
-      title: "Employee 💼 Roster",
-      githubLink: "https://github.com/MichelleMcConville/10-employee-summary",
-      subText: "Click to get code",
-      desc: "A CLI app for generating a software engineering team roster!"
-    },
-    {
-      url: "https://github.com/MichelleMcConville/09-readme-generator",
-      imageLink: "./images/comingSoon320.jpg",
-      alt: "readme-generator",
-      title: "README.md ⚙️ Generator",
-      githubLink: "https://github.com/MichelleMcConville/09-readme-generator",
-      subText: "Click to get code",
-      desc: "Create your README.md automatically!"
-    },
-    {
-      url: "https://michellemcconville.github.io/06-weather-dashboard/",
-      imageLink: "./images/weather320.jpg",
-      alt: "weather-app",
-      title: "Weather 🌤️ Dashboard",
-      githubLink: "https://github.com/MichelleMcConville/06-weather-dashboard",
-      subText: "Click to get code",
-      desc: "Check today's weather & the 5-Day forecast for a city near you!"
-    },
-    {
-      url: "https://michellemcconville.github.io/05-work-day-scheduler/",
-      imageLink: "./images/dayPlanner320.jpg",
-      alt: "day-planner-app",
-      title: "Day 📆 Planner",
-      githubLink: "https://github.com/MichelleMcConville/05-work-day-scheduler",
-      subText: "Click to get code",
-      desc: "Keep track of your workday schedule with this handy day planner!"
-    },
-    {
-      url: "https://michellemcconville.github.io/04-code-quiz/",
-      imageLink: "./images/quizStart320.jpg",
-      alt: "code-quiz",
-      title: "Code ⏲️ Quiz",
-      githubLink: "https://github.com/MichelleMcConville/04-code-quiz",
-      subText: "Click to get code",
-      desc: "Test your knowledge against your friends with this fun code quiz!"
-    },
-    {
-      url: "https://michellemcconville.github.io/03-password-generator/",
-      imageLink: "./images/pwGen320.jpg",
-      alt: "password-generator-app",
-      title: "Password 🔒 Generator",
-      githubLink: "https://github.com/MichelleMcConville/03-password-generator",
-      subText: "Click to get code",
-      desc: "Need help generating a password? Use this app to create a secure password!"
-    },
-    {
-      url: "https://michellemcconville.github.io/bookmarks/",
-      imageLink: "./images/bookmarksDark.jpg",
-      alt: "html-bookmarks",
-      title: "Favorites 🌸 Page",
-      githubLink: "https://github.com/MichelleMcConville/bookmarks",
-      subText: "Click to get code",
-      desc: "A template to use for organizing all your bookmarked sites into a fun 1-page format!"
-    }
-  ];
+  {
+    url: "https://mkkp-project2.herokuapp.com/",
+    imageLink: "./images/camping320.jpg",
+    alt: "tp-finding-app",
+    title: "Toilet 🚽 Paper Location App",
+    githubLink: "https://github.com/MichelleMcConville/P2-tpoops",
+    subText: "Click to get code",
+    desc: "Annoyed you cannot find any TP, use this app if you have located or in search of this priceless commodity."
+  },
+  {
+    url: "https://michellemcconville.github.io/P1-camping-spot-finder/",
+    imageLink: "./images/camping320.jpg",
+    alt: "camping-app",
+    title: "Camping Spot 🏕️Finder",
+    githubLink: "https://github.com/MichelleMcConville/P1-camping-spot-finder",
+    subText: "Click to get code",
+    desc: "Looking for a place to camp in the National Parks! Use this app."
+  },
+  {
+    url: "https://mchel-burger-app.herokuapp.com/",
+    imageLink: "./images/burgerApp320.jpg",
+    alt: "burger-logger",
+    title: "Burger 🍔 Logger",
+    githubLink: "https://github.com/MichelleMcConville/13-burger",
+    subText: "Click to get code",
+    desc: "Keep track of any & all burgers you have tried!"
+  },
+  {
+    url: "https://github.com/MichelleMcConville/12-employee-tracker",
+    imageLink: "./images/employeeTracker320.jpg",
+    alt: "employee-list",
+    title: "Employee 🧑‍🤝‍🧑 Tracker",
+    githubLink: "https://github.com/MichelleMcConville/12-employee-tracker",
+    subText: "Click to get code",
+    desc: "Use this CLI app to maintain a list of employees for your company!"
+  },
+  {
+    url: "https://mchel-note-taker.herokuapp.com/",
+    imageLink: "./images/noteTaker320.jpg",
+    alt: "note-taker-app",
+    title: "Note 📝 Taker",
+    githubLink: "https://github.com/MichelleMcConville/11-note-taker",
+    subText: "Click to get code",
+    desc: "Save paper by using this cute little notes app!"
+  },
+  {
+    url: "https://github.com/MichelleMcConville/10-employee-summary/",
+    imageLink: "./images/employeeSummary320.jpg",
+    alt: "team-list",
+    title: "Employee 💼 Roster",
+    githubLink: "https://github.com/MichelleMcConville/10-employee-summary",
+    subText: "Click to get code",
+    desc: "A CLI app for generating a software engineering team roster!"
+  },
+  {
+    url: "https://github.com/MichelleMcConville/09-readme-generator",
+    imageLink: "./images/comingSoon320.jpg",
+    alt: "readme-generator",
+    title: "README.md ⚙️ Generator",
+    githubLink: "https://github.com/MichelleMcConville/09-readme-generator",
+    subText: "Click to get code",
+    desc: "Create your README.md automatically!"
+  },
+  {
+    url: "https://michellemcconville.github.io/06-weather-dashboard/",
+    imageLink: "./images/weather320.jpg",
+    alt: "weather-app",
+    title: "Weather 🌤️ Dashboard",
+    githubLink: "https://github.com/MichelleMcConville/06-weather-dashboard",
+    subText: "Click to get code",
+    desc: "Check today's weather & the 5-Day forecast for a city near you!"
+  },
+  {
+    url: "https://michellemcconville.github.io/05-work-day-scheduler/",
+    imageLink: "./images/dayPlanner320.jpg",
+    alt: "day-planner-app",
+    title: "Day 📆 Planner",
+    githubLink: "https://github.com/MichelleMcConville/05-work-day-scheduler",
+    subText: "Click to get code",
+    desc: "Keep track of your workday schedule with this handy day planner!"
+  },
+  {
+    url: "https://michellemcconville.github.io/04-code-quiz/",
+    imageLink: "./images/quizStart320.jpg",
+    alt: "code-quiz",
+    title: "Code ⏲️ Quiz",
+    githubLink: "https://github.com/MichelleMcConville/04-code-quiz",
+    subText: "Click to get code",
+    desc: "Test your knowledge against your friends with this fun code quiz!"
+  },
+  {
+    url: "https://michellemcconville.github.io/03-password-generator/",
+    imageLink: "./images/pwGen320.jpg",
+    alt: "password-generator-app",
+    title: "Password 🔒 Generator",
+    githubLink: "https://github.com/MichelleMcConville/03-password-generator",
+    subText: "Click to get code",
+    desc: "Need help generating a password? Use this app to create a secure password!"
+  },
+  {
+    url: "https://michellemcconville.github.io/bookmarks/",
+    imageLink: "./images/bookmarksDark.jpg",
+    alt: "html-bookmarks",
+    title: "Favorites 🌸 Page",
+    githubLink: "https://github.com/MichelleMcConville/bookmarks",
+    subText: "Click to get code",
+    desc: "A template to use for organizing all your bookmarked sites into a fun 1-page format!"
+  }
+];
   
     // {
     //   "url: "TBD",
